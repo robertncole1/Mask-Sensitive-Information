7e4a48037bae3714d86d54221ada72256a3e0600
Rob Cole <robertncole1@gmail.com>
2021-07-21 19:15:01 -0400

    Add project files.

---
+++ Mask Sensitive Information/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Mask_Sensitive_Information
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Tell us a secret!");
+            var secret = Console.ReadLine();
+
+            var tellMe = secret.Length - 4;
+            var lastFour = secret.Remove(0, tellMe);
+            var mask = secret.Remove(tellMe);
+            var changeLetters = new System.Text.StringBuilder();
+
+            for (var i = 0; i < mask.Length; i++)
+            {
+                changeLetters.Append("$");
+            }
+            Console.WriteLine(changeLetters + lastFour);
+        }
+    }
+}
